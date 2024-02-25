b7c0e0ff3d6f035a164225f2e1d143f008b18216
Hokki Suwanda <hokki.suwanda@nri.co.id>
2024-02-25 21:10:09 +0700

    feat: wrap use-floating to add devtools

--- apps/react-template/src/main.tsx
+++ apps/react-template/src/main.tsx
@@ -1,12 +1,16 @@
 import './inject-dependencies';
 import './styles.css';
 
+import type { ReactQueryDevtools as TanstackReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { ErrorBoundary } from '@monorepo/ui';
 import noop from 'lodash/fp/noop';
-import { StrictMode } from 'react';
+import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import envConfig from './configs/env/env.config';
+import queryClient from './configs/react-query/react-query.config';
 import routeMap from './configs/route-map/route-map.config';
 import loginRoute from './pages/login/login.route';
 import mainRoute from './pages/main/main.route';
@@ -17,6 +21,10 @@ import { resolveRoute } from './utils/router/router.util';
 const rootElement = document.getElementById('root');
 
 const router = createBrowserRouter([mainRoute, loginRoute].map((route) => resolveRoute(route, routeMap)));
+const ReactQueryDevtools =
+  envConfig.env === 'development'
+    ? lazy(() => import('@tanstack/react-query-devtools').then((mod) => ({ default: mod.ReactQueryDevtools })))
+    : ((() => null) as typeof TanstackReactQueryDevtools);
 
 if (rootElement) {
   const root = createRoot(rootElement);
@@ -24,6 +32,7 @@ if (rootElement) {
   root.render(
     <StrictMode>
       <ErrorBoundary fallback="An error happened!" onError={noop}>
+        <ReactQueryDevtools client={queryClient} />
         <RouterProvider router={router} />
       </ErrorBoundary>
     </StrictMode>,
--- libs/ui/src/hooks/index.ts
+++ libs/ui/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useDebounced } from './use-debounced/use-debounced.hook';
+export { default as useFloating } from './use-floating/use-floating.hook';
 export { default as useInterval } from './use-interval/use-interval.hook';
 export { default as useMergeRef } from './use-merge-ref/use-merge-ref.hook';
 export { default as usePrevious } from './use-previous/use-previous.hook';
