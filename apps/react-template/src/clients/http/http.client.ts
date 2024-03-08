0b4b000aec30a6478940b43ca54d6ac5c2a09e35
Hokki Suwanda <hokki.suwanda@nri.co.id>
2024-03-08 22:10:08 +0700

    fix: imports

--- apps/react-template/src/clients/http/http.client.ts
+++ apps/react-template/src/clients/http/http.client.ts
@@ -1,6 +1,6 @@
 import type { IHttpClient } from '@monorepo/shared';
 
-import AxiosHttpClient from './axios/axios.http-client';
+import AxiosHttpClient from './axios/axios.http.client';
 
 const httpClient: IHttpClient = new AxiosHttpClient({});
 
--- libs/shared/src/clients/storage/index.ts
+++ libs/shared/src/clients/storage/index.ts
@@ -1,4 +1,4 @@
-export { default as IndexedDBClient } from './indexed-db.storage-client';
-export { default as LocalStorageClient } from './local.storage-client';
-export { default as SessionStorageClient } from './session.storage-client';
+export { default as IndexedDBClient } from './indexed-db.storage.client';
+export { default as LocalStorageClient } from './local.storage.client';
+export { default as SessionStorageClient } from './session.storage.client';
 export type { default as IStorageClient } from './storage.client.interface';
