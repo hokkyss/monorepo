b9bc6242018ac400785a7ac4a1a4bfb00a43a4cf
Hokki Suwanda <hokki.suwanda@nri.co.id>
2024-03-09 15:20:35 +0700

    fix: build error

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,5 +1,3 @@
 export * from './clients/http';
-export * from './clients/implementation/http';
-export * from './clients/implementation/storage';
 export * from './clients/storage';
 export * from './types/shared.type';
